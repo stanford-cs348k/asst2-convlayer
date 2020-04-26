a5c7a600a9543d87855a32ec55ec867ad0e19002
Dillon Huff <dillonhuff@gmail.com>
2020-04-26 13:10:34 -0700

    added comparison of reference and fast

--- src/convlayer_main.cpp
+++ src/convlayer_main.cpp
@@ -20,6 +20,13 @@ float* FillZero(const int size) {
   return buf;
 }
 
+void CompareBuffers(float* ref, float* data, float eps, int size) {
+  for (int i = 0; i < size; i++) {
+    float diff = abs(ref[i] - data[i]);
+    assert(diff < eps);
+  }
+}
+
 float* FillRandom(const int size) {
   auto buf = new float[size];
   for (int i = 0; i < size; i++) {
@@ -30,38 +37,60 @@ float* FillRandom(const int size) {
 
 int main(int argc, char** argv) {
   ConvolutionLayer::Parameters params;
-  params.num_f = 32;
+  params.num_f = 64;
   params.f_w = 8;
   params.f_h = 14;
   params.channels = 64;
-  params.width = 32;
+  params.width = 47;
   params.height = 32;
   params.n = 7;
   params.pad = 2;
 
   ConvolutionLayer::Data data;
-
   data.biases = FillRandom(params.num_f);
   data.weights = FillRandom(params.f_h*params.f_w*params.channels*params.num_f);
   data.input = FillRandom(params.width*params.height*params.channels*params.n);
   data.output = FillZero(params.width*params.height*params.n*params.num_f);
 
-  //std::unique_ptr<ConvolutionLayer> conv_layer(new FastConvolutionLayer);
-  std::unique_ptr<ConvolutionLayer> reference_conv_layer(new HalideConvolutionLayer);
-  reference_conv_layer->Init(params);
+  ConvolutionLayer::Data fast_data;
+  fast_data.biases = data.biases;
+  fast_data.weights = data.weights;
+  fast_data.input = data.input;
+  fast_data.output = FillZero(params.width*params.height*params.n*params.num_f);
 
-  double total_elapsed = 0.;
-  auto start = std::chrono::system_clock::now();
-  reference_conv_layer->Run(params, data);
-  auto end = std::chrono::system_clock::now();
-  std::chrono::duration<double> elapsed = end - start;
-  total_elapsed += elapsed.count();
-  std::cout << "Reference Convolution layer took " << elapsed.count() << " secconds" << std::endl;
-  cout << "Output..." << endl;
-  int out_size = params.width*params.height*params.num_f*params.n;
-  for (int i = 0; i < out_size; i++) {
-    cout << "  " << data.output[i] << endl;
+  {
+    std::unique_ptr<ConvolutionLayer> reference_conv_layer(new HalideConvolutionLayer);
+    reference_conv_layer->Init(params);
+
+    double total_elapsed = 0.;
+    auto start = std::chrono::system_clock::now();
+    reference_conv_layer->Run(params, data);
+    auto end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed = end - start;
+    total_elapsed += elapsed.count();
+    std::cout << "Reference Convolution layer took " << elapsed.count() << " secconds" << std::endl;
   }
+
+
+  {
+    std::unique_ptr<ConvolutionLayer> fast_conv_layer(new FastConvolutionLayer);
+    fast_conv_layer->Init(params);
+
+    double total_elapsed = 0.;
+    auto start = std::chrono::system_clock::now();
+    fast_conv_layer->Run(params, fast_data);
+    auto end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed = end - start;
+    total_elapsed += elapsed.count();
+    std::cout << "Fast Convolution layer took " << elapsed.count() << " secconds" << std::endl;
+  }
+
+  int output_size =
+    params.width*params.height*params.n*params.num_f;
+  float eps = 0.00001;
+  CompareBuffers(data.output, fast_data.output, eps, output_size);
+
+  std::cout << "Fast result matches reference" << endl;
 
   delete data.input;
   delete data.output;
--- src/fast_convolution_layer.cpp
+++ src/fast_convolution_layer.cpp
@@ -3,6 +3,8 @@
 #include <cmath>
 #include <iostream>
 
+#include "Halide.h"
+
 void FastConvolutionLayer::Init(Parameters params) {
   // BEGIN: CS348V STUDENTS MODIFY THIS CODE
   // END: CS348V STUDENTS MODIFY THIS CODE
@@ -10,5 +12,44 @@ void FastConvolutionLayer::Init(Parameters params) {
 
 void FastConvolutionLayer::Run(Parameters params, Data data) {
   // BEGIN: CS348V STUDENTS MODIFY THIS CODE
+  Halide::Buffer<float> in_func(data.input,
+                              params.width,
+                              params.height,
+                              params.channels,
+                              params.n);
+
+  Halide::Buffer<float> b(data.biases, params.num_f);
+  Halide::Buffer<float> W(data.weights, params.f_w, params.f_h, params.channels, params.num_f);
+
+  Halide::Func forward("conv");
+  Halide::Var x("x"), y("y"), z("z"), n("n");
+
+  Halide::Func f_in_bound;
+  f_in_bound = Halide::BoundaryConditions::repeat_edge(in_func, 0, params.width, 0, params.height, 0, params.channels, 0, params.n);
+
+  Halide::RDom r(0, params.f_w, 0, params.f_h, 0, params.channels);
+
+  forward(x, y, z, n) = b(z);
+  forward(x, y, z, n) += W(r.x, r.y, r.z, z) *
+    f_in_bound(x + r.x - params.pad, y + r.y - params.pad, r.z, n);
+
+  // Realize output buffer and copy to data.output pointer.
+  Halide::Buffer<float> output_buffer(params.width, params.height, params.num_f, params.n);
+  forward.realize(output_buffer);
+  std::cout << "[DEBUG] output buffer size = "
+            << output_buffer.get()->number_of_elements() << std::endl;
+  // Copy data from Halide buffer to raw buffer in data.output.
+  {
+    int index = 0;
+    for (int n = 0; n < params.n; n++) {
+      for (int c = 0; c < params.num_f; c++) {
+        for (int j = 0; j < params.height; j++) {
+          for (int i = 0; i < params.width; i++) {
+            data.output[index++] = output_buffer(i, j, c, n);
+          }
+        }
+      }
+    }
+  }
   // END: CS348V STUDENTS MODIFY THIS CODE
 }
