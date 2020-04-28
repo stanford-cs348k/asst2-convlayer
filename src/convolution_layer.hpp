dd4edc6fa730e0adf5672ea23c254efebddf13e9
Kayvon Fatahalian <kayvonf@gmail.com>
2020-04-27 17:38:56 -0700

    Update convolution_layer.hpp

--- src/convolution_layer.hpp
+++ src/convolution_layer.hpp
@@ -16,17 +16,17 @@ class ConvolutionLayer {
 
   // Hyperparameters for a convolution layer
   struct Parameters {
-    int width = 0;
+   
+    // input to the conv layer is: width x height x channels x n
+    int width = 0;     
     int height = 0;
-    int channels = 0;
-    int n = 0;
+    int channels = 0;   // number of channels per image (e.g., 3 for an rgb image)
+    int n = 0;          // number of images in the batch
 
-    int num_f = 0;
-    int f_h = 0;
-    int f_w = 0;
-
-    //int in_w = 0;
-    //int in_h = 0;
+    // output of the conv layer is: width x height x num_f
+    int num_f = 0;      // number of filters
+    int f_h = 0;        // height of filter kernel (e.g., f_w=3 and f_h=3 for a 3x3 conv layer)
+    int f_w = 0;        // width of filter kernel
 
     int pad = 0;
   };
